50842ce30d5dadd6217991ec65384ce338b96bc4
nazar <martynnazar11@gmail.com>
2025-02-09 16:28:27 +0200

    py

--- menu.py
+++ menu.py
@@ -10,29 +10,45 @@ def menu_window():
     right_answer = QLabel("Правильна відповідь")
     right_input = QLineEdit()
     wrong_answer1 = QLabel("Неправильна відповідь")
-    wrong_input = QLineEdit()
+    wrong_input1 = QLineEdit()
     wrong_answer2 = QLabel("Неправильна відповідь")
+    wrong_input2 = QLineEdit()
     wrong_answer3 = QLabel("Неправильна відповідь")
+    wrong_input3 = QLineEdit()
 
     add_lbl = QPushButton("Додати")
 
 
     main_line = QVBoxLayout()
 
+    h2 = QHBoxLayout()
+    h2.addWidget(right_answer)
+    h2.addWidget(right_input)
 
     h1 = QHBoxLayout()
     h1.addWidget(quest_lbl)
     h1.addWidget(quest_input)
     main_line.addLayout(h1)
 
-    h2 = QHBoxLayout()
-    h2.addWidget(right_answer)
-    h2.addWidget(right_input)
-    h2.addWidget(wrong_answer1)
-    h2.addWidget(wrong_answer2)
-    h2.addWidget(wrong_answer2)
-    h2.addWidget(wrong_input)
+
+
+
+    h3 = QHBoxLayout()
+    h3.addWidget(wrong_answer1)
+    h3.addWidget(wrong_input1)
+
+    h4 = QHBoxLayout()
+    h4.addWidget(wrong_answer2)
+    h4.addWidget(wrong_input2)
+
+    h5 = QHBoxLayout()
+    h5.addWidget(wrong_answer3)
+    h5.addWidget(wrong_input3)
+
     main_line.addLayout(h2)
+    main_line.addLayout(h5)
+    main_line.addLayout(h3)
+    main_line.addLayout(h4)
     main_line.addWidget(add_lbl)
 
 
@@ -40,7 +56,7 @@ def menu_window():
 
 
     def add_func():
-        pityh.questions.append(
+        database.questions.append(
             {
        "Запитання": quest_lbl.text(),
        "Відповідь": right_answer.text(),
